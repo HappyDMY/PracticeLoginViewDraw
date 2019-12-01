c3a02cc1c58cfdf8c0c13ef4d57e1534ec3c3a99
jun <vagrantsj@gmail.com>
2019-12-01 13:34:45 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/practiceloginviewdraw/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.tjoeun.practiceloginviewdraw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val id = inputId.text.toString()
+            val pw = inputPw.text.toString()
+
+            if(id == "admin" ){
+                if( pw == "123"){
+                    Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+
+                }else{
+                    Toast.makeText(this, "비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "일반사용자입니다.", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+
+
+
+    }
+}
